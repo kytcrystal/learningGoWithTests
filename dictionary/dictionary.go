db7c0221656040c679e99c5ce2f3ed2d75095636
Crystal Kwok <kwokyatingcrystal@gmail.com>
2023-11-15 21:03:55 +0100

    add dictionary package

---
+++ dictionary/dictionary.go
@@ -0,0 +1,54 @@
+package dictionary
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound   		= DictionaryErr("WORD NOT FOUND")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word string, translation string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = translation
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word string, newTranslation string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newTranslation
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d,word)
+}
